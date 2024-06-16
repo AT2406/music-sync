6172dbcda7c36f45872f0ebdedb79172b9b6f88f
Ali T <ali@turk.bh>
2024-06-16 20:52:12 +0100

    moved secrets to env file

--- src/index.ts
+++ src/index.ts
@@ -39,9 +39,10 @@ const requestHandler = (req: IncomingMessage, res: ServerResponse): void => {
 const server = createServer(requestHandler)
 
 server.listen(port, async () => {
-  console.log(`Server started at http://localhost:${port}`)
-  // Dynamically import and use the `open` module
-  // const open = (await import('open')).default
-  // await open(authUrl, { app: { name: 'google chrome' } })
+  console.log(`Server started at ${redirectUri}`)
   console.log('auth url:', authUrl)
 })
+
+// Dynamically import and use the `open` module
+const open = (await import('open')).default
+await open(authUrl, { app: { name: 'google chrome' } })
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const clientInformation = {
-  clientId: '0799d3758a7e4c4aa97d4e71c838d4c6',
-  clientSecret: 'fd5f5e19365a48dfabee93e5459b9426',
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
 }
 
 export const spotifyEndpoints = {
